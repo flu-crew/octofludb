e9fe3114ee5bed43bd1f8d56b054c6b31ba56358
Zebulun Arendsee <arendsee@iastate.edu>
2019-06-18 10:45:34 -0500

    Rename args

--- d79.py
+++ d79.py
@@ -11,12 +11,14 @@ Usage:
   d79 load_excel <table_filename> [<db>] [--event=<event>] [--rdf=<rdf>] [--format=<format>]
   d79 load_gbids <gb_list_filename> [<db>] [--rdf=<rdf>] [--format=<format>]
   d79 load_blast <blast_filename> [<db>] [--event=<event>] [--rdf=<rdf>] [--format=<format>]
+  d79 load_fasta <fasta_filename> [<db>] [--event=<event>] [--rdf=<rdf>] [--format=<format>] [--columns=<columns>] [--delimiter=<del>]
   d79 serialize <serial_filename> <db> [--format=<format>]
 
 Options:
-  -h --help             Show this screen.
-  -k --key-type <key>   The subject type to merge on [default="strain"].
-  -f --format <format>  The RDF format ("turtle" or "ntriples")
+  -h --help               Show this screen.
+  -k --key-type <key>     The subject type to merge on [default="strain"].
+  -f --format <format>    The RDF format ("turtle" or "ntriples")
+  -c --columns <columns>  Columns of a table for fields in a header (e.g., "sid[host,date,gid],gid[clade],host,date,clade|(sid,type,strain_id);(gid,type,barcode);(gid,type,genbank_id)"
 """
 
 import sys
@@ -29,24 +31,24 @@ import src.genbank as gb
 
 if __name__ == '__main__':
 
-  arguments = docopt(__doc__, version='build.sh 0.0.1')
+  args = docopt(__doc__, version='build.sh 0.0.1')
 
-  if arguments["<db>"]:
+  if args["<db>"]:
     g = ConjunctiveGraph(store="Sleepycat")
-    g.open(arguments["<db>"], create=True)
+    g.open(args["<db>"], create=True)
   else:
     g = ConjunctiveGraph()
 
-  if arguments["load_strains"]:
+  if args["load_strains"]:
     # load big table from IVR, with roughly the following format:
     # (gb | host | ? | subtype | date | ? | "Influenza A virus (<strain>(<subtype>))" | ...)
-    recipe.load_influenza_na(g, arguments["<table_filename>"])
+    recipe.load_influenza_na(g, args["<table_filename>"])
 
-  if arguments["tag_strains"]:
-    recipe.tag_strains(g, arguments["<idlist_filename>"], arguments["<tag>"])
+  if args["tag_strains"]:
+    recipe.tag_strains(g, args["<idlist_filename>"], args["<tag>"])
 
-  if arguments["load_gbids"]:
-    with open(arguments["<gb_list_filename>"], "r") as f:
+  if args["load_gbids"]:
+    with open(args["<gb_list_filename>"], "r") as f:
       gbids = [g.strip() for g in f.readlines()]
       for gb_metas in entrez.get_gbs(gbids):
         for gb_meta in gb_metas:
@@ -55,31 +57,34 @@ if __name__ == '__main__':
         g.commit()
         print(f' > uploaded and committed {len(gb_metas)} ids', file=sys.stderr)
 
-  if arguments["load_excel"]:
-    recipe.load_excel(g, arguments["<table_filename>"], event=arguments["--event"])
+  if args["load_excel"]:
+    recipe.load_excel(g, args["<table_filename>"], event=args["--event"])
 
-  if arguments["load_blast"]:
-    recipe.load_blast(g, arguments["<blast_filename>"], event=arguments["--event"])
+  if args["load_blast"]:
+    recipe.load_blast(g, args["<blast_filename>"], event=args["--event"])
 
-  if arguments["load_factor"]:
+  if args["load_fasta"]:
+    recipe.load_fasta(g, args["<fasta_filename>"], event=args["--event"], columns=args["--columns"], delimiter=args["--delimiter"])
+
+  if args["load_factor"]:
     recipe.load_factor(
       g,
-      arguments["<table_filename>"],
-      arguments["<relation>"],
-      arguments["--key-type"]
+      args["<table_filename>"],
+      args["<relation>"],
+      args["--key-type"]
     )
 
-  if arguments["serialize"]:
+  if args["serialize"]:
     g.serialize(
-      destination=arguments["<serial_filename>"],
-      format=arguments["--format"],
+      destination=args["<serial_filename>"],
+      format=args["--format"],
       encoding="utf-8"
     )
 
-  if arguments["--rdf"]:
+  if args["--rdf"]:
     g.serialize(
-      destination=arguments["--rdf"],
-      format=arguments["--format"],
+      destination=args["--rdf"],
+      format=args["--format"],
       encoding="utf-8"
     )
 
