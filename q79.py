643b2f06e76f6f124a727772a8a4dc552bc31343
Zebulun Arendsee <arendsee@iastate.edu>
2019-06-28 16:18:10 -0500

    Make CLI query interface

--- q79.py
+++ q79.py
@@ -3,62 +3,47 @@
 """
 Query the graph database with SPARQL files
 
-If --as-fasta is used, the first (n-1) rows will be treated as '|'-separated
-elements of the header. The nth row must contain raw sequence. The sequence
-will all be on one line. If you want to wrap it, I suggest piping to `smof
-clean`.
-
 Usage:
-  sparql.py <rdf_db> <sparql_query> [--as-fasta]
+  q79.py <sparql_query> [--repo=<repo>] [--header] [--fasta]
 
 Options:
   -h --help     Show this screen.
-  --as-fasta    Create a fasta file as output.
+  --repo <repo> GraphDB repo name
+  --header      Print a header
+  --fasta       Write fasta, where last variable is assumed to be the sequence
 """
 
-from rdflib import ConjunctiveGraph
-from rdflib.plugins.sparql import prepareQuery
-from src.nomenclature import nt
+from SPARQLWrapper import SPARQLWrapper, JSON
 from docopt import docopt
-import sys
-from rdflib.namespace import RDF, FOAF, XSD
-
-
-def load_rdf(filename):
-    with open(filename, "r") as f:
-        sparql_str = f.read().strip()
-    return sparql_str
-
 
 if __name__ == "__main__":
 
-    arguments = docopt(__doc__, version="sparql.sh 0.0.1")
+    args = docopt(__doc__, version="sparql.sh 0.0.1")
 
-    g = ConjunctiveGraph()
-    g.parse(arguments["<rdf_db>"])
-
-    if len(g) == 0:
-        print("Loaded an empty database", file=sys.stderr)
-
-    rdf_str = load_rdf(arguments["<sparql_query>"])
-
-    qres = g.query(rdf_str, initNs={"foaf": FOAF, "usda": nt, "xsd": XSD})
-
-    # Set empty fields to "-". Empty fields occur when a SPARQL query uses OPTIONAL.
-    def to_str(x):
-        if x == None:
-            return "-"
-        else:
-            return str(x)
-
-    if arguments["--as-fasta"]:
-        for row in qres:
-            seq = row[-1]
-            header = "|".join((to_str(x) for x in row[0 : (len(row) - 1)]))
-            print(f">{header}")
-            print(seq)
+    if args["--repo"]:
+        repo = args["--repo"]
     else:
-        for row in qres:
-            print("\t".join((to_str(x) for x in row)))
+        repo = "flu"
 
-    g.close()
+    with open(args["<sparql_query>"], "r") as fh:
+        sparql_file = fh.read()
+
+    sparql = SPARQLWrapper(f'http://localhost:7200/repositories/{repo}')
+    sparql.setQuery(sparql_file)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    if args["--fasta"]:
+        header_fields = results["head"]["vars"][:-1]
+        seq_field = results["head"]["vars"][-1]
+        for row in results["results"]["bindings"]:
+            header = "|".join(row[field]["value"] for field in header_fields)
+            sequence = row[seq_field]["value"]
+            print(">" + header)
+            print(sequence)
+    else:
+        if args["--header"]:
+            print("\t".join(results["head"]["vars"]))
+        for row in results["results"]["bindings"]:
+            fields = (row[field]["value"] for field in results["head"]["vars"])
+            print("\t".join(fields))
