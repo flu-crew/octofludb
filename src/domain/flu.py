67af05b32b56a240dff18c1bbff94839d3c16fdb
Zebulun Arendsee <arendsee@iastate.edu>
2019-10-07 18:32:22 -0500

    Allow "H" in constellations

--- src/domain/flu.py
+++ src/domain/flu.py
@@ -12,7 +12,7 @@ p_NA = p.regex("N\d+")
 p_internal_gene = p.regex("PB2|PB1|PA|NP|MP|M|NS1").parsecmap(lambda x: mapreplace(x, "MP", "M"))
 p_segment = p_internal_gene ^ p_HA ^ p_NA ^ p.string("HA") ^ p.string("NA")
 p_subtype = p.regex("H\d+N\d+(v)?")
-p_constellation = p.regex("[TPV-]{6}")
+p_constellation = p.regex("[TPVH-]{6}")
 p_segment_number = p.regex("[1-8]")
 
 def mapreplace(x, pattern, replace):
