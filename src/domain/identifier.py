bc06a9f26860ffa8b1f07c2402805060049621ab
Zebulun Arendsee <arendsee@iastate.edu>
2020-06-27 14:03:46 -0500

    Allow tailing space in strain names

--- src/domain/identifier.py
+++ src/domain/identifier.py
@@ -3,7 +3,7 @@ import parsec as p
 p_A0 = p.regex("A0\d{7}")
 p_tosu = p.regex("\d+TOSU\d+")
 p_epi_isolate = p.regex("EPI_ISL_\d+")
-p_strain = p.regex("[ABCD]/[^()\[\]]+")
+p_strain = p.regex("[ABCD]/[^()\[\]]+").parsecmap(lambda x: x.strip())
 p_barcode = p_A0 ^ p_tosu ^ p_epi_isolate ^ p_strain  # e.g. A01104095 or 16TOSU4783
 
 p_gb = p.regex("[A-Z][A-Z]?\d{5,7}")
