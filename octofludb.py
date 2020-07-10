77ae9ec4424f067b07300f34cafab1e53dd65522
Zebulun Arendsee <arendsee@iastate.edu>
2020-07-09 19:39:23 -0500

    Replace docopt with argparse (need testing)

--- octofludb.py
+++ octofludb.py
@@ -1,208 +1,359 @@
 #!/usr/bin/env python3
 
-
-"""
-Build a local SPARQL database.
-
-Usage:
-  octofludb query [<sparql_filename>] [--header] [--fasta] [--url=<url>] [--repo=<repo>]
-  octofludb update [<sparql_filename>] [--url=<url>] [--repo=<repo>]
-  octofludb upload [<turtle_filenames>...] [--url=<url>] [--repo=<repo>]
-  octofludb mk_ivr [<filename>]
-  octofludb mk_ird [<filename>]
-  octofludb mk_gis [<filename>]
-  octofludb tag [<filename>] [--tag=<tag>]
-  octofludb mk_gbids [<filename>]
-  octofludb mk_gbank [<filename>]
-  octofludb mk_blast [<filename>] [--tag=<tag>]
-  octofludb mk_table [<filename>] [--tag=<tag>] [--include=<inc>] [--exclude=<exc>] [--levels=<levels>] [--na=<na_str>]
-  octofludb mk_fasta [<filename>] [--tag=<tag>] [--delimiter=<del>] [--include=<inc>] [--exclude=<exc>] [--na=<na_str>]
-
-Options:
-  -h --help               Show this screen.
-  -k --key-type <key>     The subject type to merge on [default: "strain"]
-  --delimiter <del>       Field delimiter for FASTA headers [default: "|"]
-  --include <inc>         Only parse using these tokens (comma-delimited list) [default: ""]
-  --exclude <exc>         Remove these tokens (comma-delimited list) [default: ""]
-  --header                Include a header of column names indata returned from query
-  --fasta                 Return query as a fasta file where last column is sequence
-  --url <url>             GraphDB url [default: http://localhost:7200]
-  --repo <repo>           GraphDB repo name [default: flu]
-"""
-
 import os
+import sys
 import signal
 import pgraphdb as db
-from docopt import docopt
+import argparse
+import textwrap
+import src.cli as cli
 
-if __name__ == "__main__":
 
-    if os.name is "posix":
-        signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+parser = argparse.ArgumentParser(
+    prog="octofludb",
+    formatter_class=cli.SubcommandHelpFormatter,
+    description="API and utilities for the swine flu surveillance database",
+    epilog=textwrap.dedent(
+        "Submit issue reports to https://github.com/flu-crew/octofludb/issues"
+    ),
+)
+subparsers = parser.add_subparsers(metavar="<subcommand>", title="subcommands")
+subcommand = cli.subcommand_maker(subparsers)
 
-    args = docopt(__doc__, version="octofludb 0.2.0")
+url_arg = cli.argument("--url", help="GraphDB URL", default="http://localhost:7200")
+repo_name_arg = cli.argument("--repo", help="Repository name", default="flu")
+tag_arg = cli.argument("tag", help="A tag to associate with each identifier")
 
-    # Place imports here so they only load after successful parsing of the arguments.
-    # If the imports are all before main, then `octofludb -h` can take a few seconds to run.
-    import sys
-    import shutil
-    from Bio import SeqIO
-    from rdflib import Graph, Literal
-    import src.recipes as recipe
-    import src.formatting as formatting
-    import src.entrez as entrez
-    import src.genbank as gb
-    import src.classes as classes
-    import datetime as datetime
-    from src.nomenclature import manager, make_uri, make_tag_uri, P
-    from tqdm import tqdm
+
+def open_graph():
+    from rdflib import Graph
+    from src.nomenclature import manager
+
+    return Graph(namespace_manager=manager)
+
+
+def with_graph(f, filename, *args, **kwargs):
     from src.util import log, file_str
 
-    if args["query"]:
-        results = db.sparql_query(
-            sparql_file=args["<sparql_filename>"],
-            url=args["--url"],
-            repo_name=args["--repo"],
-        )
-        if args["--fasta"]:
-            formatting.write_as_fasta(results)
-        else:
-            formatting.write_as_table(results, header=args["--header"])
+    g = open_graph()
+    with open(filename, "r") as fh:
+        f(g, fh, *args, **kwargs)
+        g.commit()  # just in case we missed anything
+        log("Serializing to turtle format ... ", end="")
+        turtles = g.serialize(format="turtle")
+        log("done")
+        for l in turtles.splitlines():
+            print(l.decode("utf-8"))
+    g.close()
 
-        sys.exit(0)
 
-    if args["update"]:
-        results = db.update(
-            sparql_file=args["<sparql_filename>"],
-            url=args["--url"],
-            repo_name=args["--repo"],
-        )
-
-    if args["upload"]:
-        for filename in args["<turtle_filenames>"]:
-            new_filename = os.path.join(
-                os.path.expanduser("~"), "graphdb-import", os.path.basename(filename)
-            )
-            if filename == new_filename:
-                continue
-            else:
-                try:
-                    shutil.copyfile(filename, new_filename)
-                except AttributeError as e:
-                    print(
-                        f"Could not move {filename} to {new_filename}: {str(e)}",
-                        file=sys.stderr,
-                    )
-                    sys.exit(1)
-
-        server_files = [os.path.basename(f) for f in args["<turtle_filenames>"]]
-
-        db.load_data(
-            url=args["--url"], repo_name=args["--repo"], turtle_files=server_files
-        )
-        sys.exit(0)
-
-    tagstr = args["--tag"]
-
-    if args["<filename>"]:
+def open_file(path):
+    if path:
         filehandle = open(args["<filename>"], "r")
     else:
         filehandle = sys.stdin
+    return filehandle
 
-    if args["--na"]:
-        na_str = args["--na"].split(",")
-        if isinstance(na_str, list):
-            na_str = [None] + na_str
+
+def make_na(na_str):
+    if na_str:
+        na_list = na_str.split(",")
+        if isinstance(na_list, list):
+            na_list = [None] + na_str
         else:
-            na_str = [None, na_str]
+            na_list = [None, na_list]
     else:
-        na_str = [None]
+        na_list = [None]
+    return na_list
 
-    g = Graph(namespace_manager=manager)
 
-    if args["mk_ivr"]:
-        # load big table from IVR, with roughly the following format:
-        # (gb | host | ? | subtype | date | ? | "Influenza A virus (<strain>(<subtype>))" | ...)
-        recipe.mk_influenza_na(g, filehandle)
+@subcommand(
+    [
+        "query",
+        cli.argument("sparql_filename"),
+        cli.argument(
+            "--header",
+            help="Include a header of column names indata returned from query",
+            action="store_true",
+            default=False,
+        ),
+        cli.argument(
+            "--fasta",
+            help="Return query as a fasta file where last column is sequence",
+            action="store_true",
+            default=False,
+        ),
+        url_arg,
+        repo_name_arg,
+    ]
+)
+def call_query_cmd(args):
+    """
+    Submit a SPARQL query to octofludb
+    """
+    import src.formatting as formatting
 
-    if args["mk_ird"]:
-        recipe.mk_ird(g, filehandle)
+    results = db.sparql_query(
+        sparql_file=args.sparql_filename, url=args.url, repo_name=args.repo
+    )
+    if args.fasta:
+        formatting.write_as_fasta(results)
+    else:
+        formatting.write_as_table(results, header=args.header)
+    sys.exit(0)
 
-    if args["mk_gis"]:
-        recipe.mk_gis(g, filehandle)
 
-    if args["tag"]:
-        taguri = make_tag_uri(tagstr)
-        g.add((taguri, P.name, Literal(tagstr)))
+@subcommand(["update", cli.argument("sparql_filename"), url_arg, repo_name_arg])
+def call_query_cmd(args):
+    """
+    Submit a SPARQL query to octofludb
+    """
+    db.update(sparql_file=args.sparql_filename, url=args.url, repo_name=args.repo)
+    return None
+
+
+@subcommand(
+    [
+        "upload",
+        cli.argument(
+            "turtle_filenames", help="One or more turtle filenames", nargs="+"
+        ),
+        url_arg,
+        repo_name_arg,
+    ]
+)
+def call_upload_cmd(args):
+    """
+    Upload one or more turtle files to the database
+    """
+    import shutil
+
+    for filename in args.turtle_filenames:
+        new_filename = os.path.join(
+            os.path.expanduser("~"), "graphdb-import", os.path.basename(filename)
+        )
+        if filename == new_filename:
+            continue
+        else:
+            try:
+                shutil.copyfile(filename, new_filename)
+            except AttributeError as e:
+                print(
+                    f"Could not move {filename} to {new_filename}: {str(e)}",
+                    file=sys.stderr,
+                )
+                sys.exit(1)
+
+    server_files = [os.path.basename(f) for f in args.turtle_filenames]
+
+    db.load_data(url=args.url, repo_name=args.repo, turtle_files=server_files)
+    sys.exit(0)
+
+
+@subcommand(["tag", cli.argument("filename", help="File of identifiers"), tag_arg])
+def tag_cmd(args):
+    """
+    Associate list of IDs with a tag
+    """
+    from rdflib import Literal
+    import datetime as datetime
+    from src.nomenclature import make_uri, make_tag_uri, P
+    from src.util import file_str
+
+    def _tag_cmd(g, fh):
+        taguri = make_tag_uri(args.tag)
+        g = open_graph()
+        g.add((taguri, P.name, Literal(args.tag)))
         g.add((taguri, P.time, Literal(datetime.datetime.now())))
         g.add((taguri, P.file, Literal(file_str(filehandle))))
-        for identifier in (s.strip() for s in filehandle.readlines()):
+        for identifier in (s.strip() for s in fh.readlines()):
             g.add((make_uri(identifier), P.tag, taguri))
-        g.commit()
 
-    if args["mk_gbids"]:
+    with_graph(_tag_cmd, args.filename)
+
+
+@subcommand(["mk_ivr", cli.argument("filename", help="The filename of an IVR table")])
+def mk_ivr_cmd(args):
+    """
+    Translate an IVR table to RDF
+
+    load big table from IVR, with roughly the following format:
+    gb | host | - | subtype | date | - | "Influenza A virus (<strain>(<subtype>))" | ...
+    """
+    import src.recipes as recipe
+
+    with_graph(recipe.mk_influenza_na, args.filename)
+
+
+@subcommand(["mk_ird", cli.argument("filename", help="The filename of an IRD table")])
+def mk_ivr_cmd(args):
+    """
+    Translate an IRD table to RDF
+    """
+    import src.recipes as recipe
+
+    with_graph(recipe.ird, args.filename)
+
+
+@subcommand(
+    ["mk_gis", cli.argument("filename", help="Path to a Gisaid metadata excel file")]
+)
+def mk_gis_cmd(args):
+    """
+    Translate a Gisaid metadata excel file to RDF
+    """
+    import src.recipes as recipe
+
+    with_graph(recipe.gis, args.filename)
+
+
+@subcommand(
+    ["mk_gbids", cli.argument("filename", help="File containing a list of genbank ids")]
+)
+def mk_gbids_cmd(args):
+    """
+    Retrieve data for a list of genbank ids
+    """
+    import src.entrez as entrez
+    import src.genbank as gb
+    from src.util import log, file_str
+
+    def _mk_gbids_cmd(g, fh):
         log(f"Retrieving and parsing genbank ids from '{file_str(filehandle)}'")
-        gbids = [g.strip() for g in filehandle.readlines()]
+        gbids = [gbid.strip() for gbid in fh.readlines()]
         for gb_metas in entrez.get_gbs(gbids):
             for gb_meta in gb_metas:
                 gb.add_gb_meta_triples(g, gb_meta)
             # commit the current batch (say of 1000 entries)
             g.commit()
 
-    if args["mk_gbank"]:
-        print("mk_gbank is not yet supported", file=sys.stderr)
-        sys.exit(1)
-        #  for gb_meta in SeqIO.parse(filehandle, "genbank"):
-        #      gb.add_gb_meta_triples(g, gb_meta)
-        #  g.commit()
+    with_graph(_mk_gbids_cmd, args.filename)
 
-    if args["mk_blast"]:
-        log(f"Retrieving and parsing blast results from '{file_str(filehandle)}'")
-        recipe.mk_blast(g, filehandle, tag=tagstr)
 
-    if args["mk_table"] or args["mk_fasta"]:
-        if not args["--include"]:
-            inc = {}
-        else:
-            inc = set(args["--include"].split(","))
-        if not args["--exclude"]:
-            exc = {}
-        else:
-            exc = set(args["--exclude"].split(","))
-        if not args["--levels"]:
-            levels = None
-        else:
-            levels = {s.strip() for s in args["--levels"].split(",")}
+@subcommand(
+    [
+        "mk_blast",
+        cli.argument("filename", help="File containing a list of genbank ids"),
+        tag_arg,
+    ]
+)
+def mk_blast_cmd(args):
+    """
+    Translate BLAST results into RDF
+    """
+    import src.recipes as recipe
+    from src.util import log, file_str
 
-        if args["mk_table"]:
-            classes.Table(
-                filehandle=filehandle,
-                tag=tagstr,
-                include=inc,
-                exclude=exc,
-                log=True,
-                na_str=na_str,
-                levels=levels,
-            ).connect(g)
+    log(f"Retrieving and parsing blast results from '{args.filename}'")
+    with_graph(recipe.mk_blast, args.filename, tag=args.tag)
 
-        if args["mk_fasta"]:
-            classes.Ragged(
-                filehandle=filehandle,
-                tag=tagstr,
-                include=inc,
-                exclude=exc,
-                log=True,
-                levels=levels,
-                na_str=na_str,
-            ).connect(g)
 
-    g.commit()  # just in case we missed anything
+def process_tablelike(include, exclude, levels, is_fasta=False):
+    if not include:
+        inc = {}
+    else:
+        inc = set(include.split(","))
+    if not exclude:
+        exc = {}
+    else:
+        exc = set(exclude.split(","))
+    if not levels:
+        levels = None
+    else:
+        levels = {s.strip() for s in levels.split(",")}
+    return (inc, exc, levels)
 
-    log("Serializing to turtle format ... ", end="")
-    turtles = g.serialize(format="turtle")
-    log("done")
-    for l in turtles.splitlines():
-        print(l.decode("utf-8"))
 
-    filehandle.close()
-    g.close()
+include_arg = cli.argument(
+    "--include", help="Only parse using these tokens (comma-delimited list)", default=""
+)
+exclude_arg = cli.argument(
+    "--exclude", help="Remove these tokens (comma-delimited list)", default=""
+)
+na_arg = cli.argument("--na", help="The string that represents a missing value")
+
+
+@subcommand(
+    [
+        "mk_table",
+        cli.argument("filename", help="Path to a table"),
+        tag_arg,
+        include_arg,
+        exclude_arg,
+        cli.argument("--levels", help="levels"),
+        na_arg,
+    ]
+)
+def mk_table_cmd(args):
+    """
+    Translate a table to RDF
+    """
+    import src.classes as classes
+
+    def _mk_table_cmd(g, fh):
+        (inc, exc, levels) = process_tablelike(
+            args.include, args.exclude, args.levels, is_fasta=False
+        )
+        classes.Ragged(
+            filehandle=fh,
+            tag=tagstr,
+            include=inc,
+            exclude=exc,
+            log=True,
+            levels=levels,
+            na_str=make_na(args.na),
+        ).connect(g)
+
+    with_graph(_mk_table_cmd, args.filename)
+
+
+@subcommand(
+    [
+        "mk_fasta",
+        cli.argument("filename", help="Path to a TAB-delimited or excel table"),
+        tag_arg,
+        cli.argument(
+            "--delimiter",
+            help="The delimiter between fields in the header",
+            default="|",
+        ),
+        include_arg,
+        exclude_arg,
+        na_arg,
+    ]
+)
+def mk_fasta_cmd(args):
+    """
+    Translate a fasta file to RDF
+    """
+    import src.classes as classes
+
+    def _mk_fasta_cmd(g, fh):
+        (inc, exc, levels) = process_tablelike(
+            args.include, args.exclude, None, is_fasta=True
+        )
+        classes.Ragged(
+            filehandle=fh,
+            tag=tagstr,
+            include=inc,
+            exclude=exc,
+            log=True,
+            levels=levels,
+            na_str=make_na(args.na),
+        ).connect(g)
+
+    with_graph(_mk_fasta_cmd, args.filename)
+
+
+def main():
+    args = parser.parse_args()
+    if len(vars(args)) == 0:
+        parser.print_help()
+    else:
+        args.func(args)
+
+
+if __name__ == "__main__":
+    if os.name is "posix":
+        signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+    main()
